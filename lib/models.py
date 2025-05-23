0cb93104435d2647aa4d0909bb5f7665a3614109
Brian <brian.sila1@student.moringaschool.com>
2025-05-23 15:10:36 +0300

    Freebie tracker

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -16,6 +17,9 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', back_populates='company')
+    devs = association_proxy('freebies', 'dev', creator= lambda dv: Freebie(dev=dv))
+
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -25,5 +29,23 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', back_populates='dev')
+    companies = association_proxy('freebies', 'company', creator= lambda cm: Freebie(company=cm))
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    company = relationship('Company', back_populates='freebies')
+    dev = relationship('Dev', back_populates='freebies')
+
+    def __repr__(self):
+        return f"<Freebie {self.id}: {self.name} Company_ID: {self.company_id}, Dev_ID: {self.dev_id}>"
+    
